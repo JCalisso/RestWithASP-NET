6ec61e253a064d729910bce1f637ca18e665a6ee
JCalisso <jv.calisso@gmail.com>
2023-07-25 20:11:59 -0300

    Implementando o Parser de Enttidade para VO e Vice Versa

---
+++ 03_RestWithASPNet_CalculatorControllersAndModels/RestWithASPNet/RestWithASPNet/Data/Converter/Contract/IParser.cs
@@ -0,0 +1,10 @@
+﻿namespace RestWithASPNet.Data.Converter.Contract
+{
+    public interface IParser<O, D>
+    {
+
+        D Parse(O origin);
+
+        List<D> Parse(List<O> origin);
+    }
+}
---
+++ 03_RestWithASPNet_CalculatorControllersAndModels/RestWithASPNet/RestWithASPNet/Data/Converter/Implementations/Converter.cs
@@ -0,0 +1,47 @@
+﻿using RestWithASPNet.Data.Converter.Contract;
+using RestWithASPNet.Data.VO;
+using RestWithASPNet.Models;
+
+namespace RestWithASPNet.Data.Converter.Implementations
+{
+    public class Converter : IParser<PersonVO, Person>, IParser<Person, PersonVO>
+    {
+        public Person Parse(PersonVO origin)
+        {
+            if (origin == null) return null;
+            return new Person
+            {
+                Id = origin.Id,
+                FirstName = origin.FirstName,
+                LastName = origin.LastName,
+                Address = origin.Address,
+                Gender = origin.Gender
+            };
+        }
+
+        public PersonVO Parse(Person origin)
+        {
+            if (origin == null) return null;
+            return new PersonVO
+            {
+                Id = origin.Id,
+                FirstName = origin.FirstName,
+                LastName = origin.LastName,
+                Address = origin.Address,
+                Gender = origin.Gender
+            };
+        }
+
+        public List<Person> Parse(List<PersonVO> origin)
+        {
+            if (origin == null) return null;
+            return origin.Select(item => Parse(item)).ToList();
+        }
+
+        public List<PersonVO> Parse(List<Person> origin)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
