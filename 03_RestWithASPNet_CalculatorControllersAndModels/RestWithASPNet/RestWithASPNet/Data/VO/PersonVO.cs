a26aab705bda652eabc9968d82a136e082a1433c
JCalisso <jv.calisso@gmail.com>
2023-07-31 11:21:07 -0300

    Custom Serialization

--- 03_RestWithASPNet_CalculatorControllersAndModels/RestWithASPNet/RestWithASPNet/Data/VO/PersonVO.cs
+++ 03_RestWithASPNet_CalculatorControllersAndModels/RestWithASPNet/RestWithASPNet/Data/VO/PersonVO.cs
@@ -1,15 +1,22 @@
-﻿namespace RestWithASPNet.Data.VO
+﻿using System.Text.Json.Serialization;
+
+namespace RestWithASPNet.Data.VO
 {
     public class PersonVO
     {
-
+        [JsonPropertyName("code")]
         public int Id { get; set; }
+
+        [JsonPropertyName("name")]
         public string FirstName { get; set; }
 
+        [JsonPropertyName("last_name")]
         public string LastName { get; set; }
 
+        [JsonIgnore]
         public string Address { get; set; }
 
+        [JsonPropertyName("sex")]
         public string Gender { get; set; }
     }
 }
