06fc100f0b470afcbb882900041d1325819bbcf9
Jean Calisso <jv.calisso@gmail.com>
2025-05-18 05:39:53 -0300

    adicionando upload e download de arquivos

---
+++ 03_RestWithASPNet_CalculatorControllersAndModels/RestWithASPNet/RestWithASPNet/Business/IFileBusiness.cs
@@ -0,0 +1,12 @@
+﻿using RestWithASPNet.Data.VO;
+
+namespace RestWithASPNet.Business
+{
+    public interface IFileBusiness
+    {
+        public byte[] GetFile(string filename);
+
+        public Task<FileDetailVO> SaveFileToDisk(IFormFile file);
+        public Task<List<FileDetailVO>> SaveFilesToDisk(IList<IFormFile> file);
+    }
+}
--- 03_RestWithASPNet_CalculatorControllersAndModels/RestWithASPNet/RestWithASPNet/Program.cs
+++ 03_RestWithASPNet_CalculatorControllersAndModels/RestWithASPNet/RestWithASPNet/Program.cs
@@ -18,6 +18,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -140,9 +141,12 @@ builder.Services.AddSwaggerGen( c =>
 });
 
 //Dependency Injection
+builder.Services.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
 builder.Services.AddScoped<IPersonBusiness, PersonBusinessImplementation>();
 builder.Services.AddScoped<IBookBusiness, BookBusinessImplementation>();
 builder.Services.AddScoped<ILoginBusiness, LoginBusinessImplementation>();
+builder.Services.AddScoped<IFileBusiness, FileBusinessImplementation>();
 
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IPersonRepository, PersonRepository>();
